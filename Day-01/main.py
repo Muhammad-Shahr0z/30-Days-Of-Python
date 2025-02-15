3c5308473d7bca60b481087a40682d83bf18e598
MuhammadShahroz <159715083+Muhammad-Shahr0z@users.noreply.github.com>
2025-02-15 14:29:20 +0500

    Day -01 Completed

---
+++ Day-01/main.py
@@ -0,0 +1,33 @@
+print("hello world")
+
+
+# - Variables & Data Types (`int`, `float`, `str`, `bool`)
+
+
+age:int = 26
+print(age, type(age))
+
+float_num:float = 5.25
+print(float_num , type(float_num))
+
+my_name:str = "Muhammad Shahroz"
+print(my_name , type(my_name))
+
+boolean:bool = 5 > 0
+print(boolean , type(boolean))
+
+
+# **Exercise:** Print your name, age, and city. Perform basic arithmetic operations.
+
+print(my_name)
+print(age)
+print("Karachi")
+
+
+print(4+4)
+print(5*4)
+print(4/1)
+print(2**2)
+print(10%1)
+
+
