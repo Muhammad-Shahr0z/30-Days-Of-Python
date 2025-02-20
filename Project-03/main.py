a1c529057618712cdfc9ad889d92eddb066009c7
MuhammadShahroz <159715083+Muhammad-Shahr0z@users.noreply.github.com>
2025-02-21 01:36:40 +0500

    Project-03 completed

---
+++ Project-03/main.py
@@ -0,0 +1,9 @@
+word = input("Enter your word to count: ")
+
+wordList= word.split()
+
+
+print(f"\nThe length of the entered word is: {len(wordList)}")
+print(f"The length of the entered Characters is: {len(word.strip())}")
+
+
